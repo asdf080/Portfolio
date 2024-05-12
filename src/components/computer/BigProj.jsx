d7b845e18da3650026e38b1a84a421d126b1a455
Dahee Lee <ai04120@naver.com>
2024-05-12 19:50:19 +0900

    1

--- src/components/computer/BigProj.jsx
+++ src/components/computer/BigProj.jsx
@@ -7,15 +7,37 @@ export default function BigProj() {
   return (
     <section className="comWindow">
       <ProjArt
+        tit="대구 원데이 클래스 사이트"
+        date="2024.04.01 ~ 2024.04.26"
+        imgs={["dongseong1.png", "dongseong2.png", "dongseong3.png"]}
+        desc="대구의 원데이 클래스를 홍보하거나, 수강 신청할 수 있는 사이트입니다. 팀 프로젝트로 제작했으며 기획, UI UX 디자인, 웹 퍼블리싱, 프론트 엔드를 담당했습니다."
+        lis={["회원 가입, 로그인 기능", "회원 전용 수강 신청 기능", "알림톡 전송"]}
+        compo={[ReacIcon, TailwindIcon]}
+        host="http://dongseong.dothome.co.kr/"
+        git="https://github.com/sikkkkkw/OneDay"
+        fig="https://www.figma.com/file/PnKLyCHOkxAUAYQZFZMgN2/one-day?type=design&mode=design&t=gJuwZCa96y3YZf6Z-0"
+      />
+      <ProjArt
         tit="동성로 관광 안내 사이트"
         date="2024.03.04 ~ 2024.03.28"
         imgs={["dongseong1.png", "dongseong2.png", "dongseong3.png"]}
-        desc="대구광역시의 동성로를 안내하는 사이트입니다. 팀 프로젝트로 제작했으며 UI UX 디자인과 웹 퍼블리싱, 프론트엔드를 담당했습니다."
+        desc="대구광역시의 동성로를 안내하는 사이트입니다. 팀 프로젝트로 제작했으며 UI UX 디자인과 웹 퍼블리싱, 프론트 엔드를 담당했습니다."
         lis={["네이버, 카카오 연동 소셜 로그인", "회원 전용 장소 찜목록 관리", "관리자 전용 회원 검색 및 삭제", "오픈 API를 연동한 정보 제공"]}
         compo={[HtmlIcon, CssIcon, JsIcon, PHPIcon]}
         host="http://dongseong.dothome.co.kr/"
         git="https://github.com/schentea/0304-NewProject"
         fig="https://www.figma.com/file/XXD4vpzMCScxfClxyP2Kq8/%EB%8F%99%EC%84%B1%EB%A1%9C?type=design&node-id=0-1&mode=design&t=gwBfhC3kYbAYkgqa-0"
+      />
+      <ProjArt
+        tit="포켓몬 도감 사이트"
+        date="2024.02.06 ~ 24.02.26"
+        imgs={["pokemon1.png", "pokemon2.png", "pokemon3.png"]}
+        desc="포켓몬 API로 만든 도감 사이트입니다."
+        lis={["Vertical Timeline로 만든 포켓몬 게임 시리즈 연대표", "Framer motion로 만든 동적 요소", "모바일, 태블릿, PC 화면을 지원하는 반응형 디자인"]}
+        compo={[ReacIcon]}
+        host="https://pokemon-08.netlify.app/"
+        git="https://github.com/asdf080/pokemon"
+        fig="https://www.figma.com/file/O9loF8apZOcka9qZ1A65IE/pokemon?type=design&node-id=0-1&mode=design&t=gJuwZCa96y3YZf6Z-0"
       />
       <ProjArt tit="Marvel Clone WEB" date="2024.01.22 ~ 2024.02.08" imgs={["marvel1.png", "marvel2.png", "marvel3.png"]} desc="Marvel 웹사이트 클론코딩입니다. " lis={["모든 화면 크기를 지원하는 반응형 디자인", "useInfiniteQuery를 활용한 무한 스크롤 UI", "React Router를 활용한 페이지 렌더링", "Framer motion을 활용한 동적 요소"]} compo={[ReacIcon, TailwindIcon]} host="https://spontaneous-meringue-3846b5.netlify.app/" git="https://github.com/asdf080/Marble-Web" />
       <ProjArt
